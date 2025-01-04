93d0ff3227fbff25d09316d3e5be977ee29e22b9
NemanjaTrninic <trninicnemanja6@gmail.com>
2025-01-04 21:47:08 +0100

    maps and breadcrumb added

--- src/app/flight/flight.component.ts
+++ src/app/flight/flight.component.ts
@@ -1,15 +1,20 @@
 import { Component, Input } from '@angular/core';
-import { ActivatedRoute, RouterModule } from '@angular/router';
+import { ActivatedRoute, RouterLink, RouterModule } from '@angular/router';
 import { FlightModel } from '../../models/flight.models';
 import { WebService } from '../../service/web.service';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgIf } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
+import {  MatButtonModule } from '@angular/material/button';
+import { SafePipe } from '../safe.pipe';
+import {  MatListModule } from '@angular/material/list';
+import { DataService } from '../../models/data.service';
+
 
 @Component({
   selector: 'app-flight',
   standalone: true,
-  imports: [HttpClientModule,NgIf,MatCardModule],
+  imports: [HttpClientModule, NgIf, MatCardModule, MatButtonModule, SafePipe, MatListModule, RouterLink],
   templateUrl: './flight.component.html',
   styleUrl: './flight.component.css'
 })
@@ -19,13 +24,20 @@ export class FlightComponent {
 
   public flight: FlightModel | undefined
 
-  constructor(private route: ActivatedRoute ) {
-    this.webService = new WebService
+  public dataService: DataService
 
+  constructor(private route: ActivatedRoute ) {
+    this.webService = new WebService()
+    this.dataService = new DataService()
     route.params.subscribe(params=>{
       this.webService.getFlightById(params['id']).subscribe(rsp=>{
         this.flight = rsp
+        
       })
     })
+  }
+
+  public generateMapLink(){
+    return 'https://www.google.com/maps?output=embed&q=' + this.flight?.destination
   }
 }
